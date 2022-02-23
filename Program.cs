0f0d4c547bda0f682a7167d3a0c0c04992fddd87
Tai55 <jesus.393hernandez@gmail.com>
2022-02-23 14:10:31 +0100

    Casi c´est fini

--- Program.cs
+++ Program.cs
@@ -1,11 +1,40 @@
-﻿
+﻿using System;
 public class Program
 {
     public static void Main(String[] args)
     {
         var generadorAleatorio = new Random();
         int numeroParaAdivinar = generadorAleatorio.Next(0, 16);
+        int numeroUsuario = 0;
+        int contador = 0;
+        const string falloNumero = "El número introducio es incorrecto";
+        const string numeroMayor = "El numero que tienes que adivinar es mayor que el elegido, pruebe a escribir un nuevo número";
+        const string numeroMenor = "El numero que tienes que adivinar es menor que el elegido, pruebe a escribir un nuevo número";
 
-        Console.WriteLine($"El numero a adivinar era {numeroParaAdivinar}");
+        Console.WriteLine("Hola! Bienvenido al adivinador de números, los primero que tendrá que hacer es escribir un número entre el 0 y el 16");
+        numeroUsuario = Int32.Parse(Console.ReadLine());
+        //poner que si es menor que 0 y mayor que 16 no lo recoja
+
+        while (contador < 2)
+        {
+            if (numeroParaAdivinar < numeroUsuario)
+            {
+                Console.WriteLine(numeroMenor);
+                numeroUsuario = Int32.Parse(Console.ReadLine());
+                contador++;
+            }
+            else if (numeroParaAdivinar > numeroUsuario)
+            {
+                Console.WriteLine(numeroMayor);
+                numeroUsuario = Int32.Parse(Console.ReadLine());
+                contador++;
+            }
+            else if (numeroParaAdivinar == numeroUsuario)
+            {
+                Console.WriteLine("GANAS");
+                contador = 2;
+            }
+            
+         }
     }
 }

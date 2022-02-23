bcd7011bdf22fea81aaa8d0a3bf4f575fb9a3019
Tai55 <jesus.393hernandez@gmail.com>
2022-02-23 18:14:34 +0100

    Juego terminado

--- Program.cs
+++ Program.cs
@@ -7,13 +7,15 @@ public class Program
         int numeroParaAdivinar = generadorAleatorio.Next(0, 16);
         int numeroUsuario = 0;
         int contador = 0;
-        const string falloNumero = "El número introducio es incorrecto";
+
+        const string falloNumero = "El número introducido es incorrecto";
         const string numeroMayor = "El numero que tienes que adivinar es mayor que el elegido, pruebe a escribir un nuevo número";
         const string numeroMenor = "El numero que tienes que adivinar es menor que el elegido, pruebe a escribir un nuevo número";
 
-        Console.WriteLine("Hola! Bienvenido al adivinador de números, los primero que tendrá que hacer es escribir un número entre el 0 y el 16");
+        Console.Title = "ADIVINADOR DE NÚMEROS";
+        Console.WriteLine("Hola! Bienvenido al adivinador de números, los primero que tendrá que hacer es escribir un número entre el 0 y el 16." +
+            "\nSe recuerda que solamente tendrá TRES intentos para adivinarlo");
         numeroUsuario = Int32.Parse(Console.ReadLine());
-        //poner que si es menor que 0 y mayor que 16 no lo recoja
 
         while (contador < 2)
         {
@@ -31,10 +33,12 @@ public class Program
             }
             else if (numeroParaAdivinar == numeroUsuario)
             {
-                Console.WriteLine("GANAS");
+                Console.WriteLine($"WIINNNER WINNER, CHICKEN DIINNNER!! ENHORABUUENA! EL NÚMERO {numeroUsuario} es el CORRECTOO!!!");
                 contador = 2;
+                Environment.Exit(0);
             }
-            
-         }
+        }
+        Console.WriteLine($"Ohhh... que lástima... Te has quedado sin intentos y el juego ha terminado. El numero a adivinar era {numeroParaAdivinar}");
+
     }
 }
